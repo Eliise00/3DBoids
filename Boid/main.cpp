3e30f9c4d947876e643830754e19cd292457a7dc
unknown <quentinhuet4500@gmail.com>
2023-03-16 12:48:50 +0100

    c cassé, à debug les parametres des boids

--- Boid/main.cpp
+++ Boid/main.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
+#include <iostream>
 #include <random>
+#include "imgui.h"
 #include "p6/p6.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT
@@ -9,72 +11,66 @@ private:
     glm::vec2 _pos;
     glm::vec2 _speed;
     float     _turn_factor;
-    // p6::Angle _dir;
-    float _radius;
-
-    float _cohesion_radius;
-    float _max_speed;
+    float     _avoid_radius;
+    float     _avoid_factor;
+    float     _align_radius;
+    float     _align_factor;
+    float     _draw_radius;
 
 public:
     explicit Boid(float aspect_ratio)
         : _pos(glm::vec2(p6::random::number(-aspect_ratio, aspect_ratio), p6::random::number(-1, 1))), // remplace seed avec la doc de p6
         _speed(p6::random::number(-.001, .001), p6::random::number(-.001, .001))
-        , _turn_factor(0.0002)
-        //, _dir(p6::Angle(p6::Radians(p6::random::number(p6::PI * 2))))
-        , _radius(.01)
-        , _cohesion_radius(.04)
-        , _max_speed(.001)
+        , _turn_factor(.0002)
+        , _avoid_radius(.06)
+        , _avoid_factor(.002)
+        , _align_radius(.2)
+        , _align_factor(.006)
+        , _draw_radius(.01)
     {
     }
 
-    void draw(p6::Context& ctx) const
+    void draw(p6::Context& ctx, bool show_avoid_circle, bool show_align_circle, float avoid_radius, float align_radius) const
     {
-        ctx.stroke_weight = 0.005f;
-        ctx.use_stroke    = false;
-        ctx.use_fill      = true;
+        // boid circle
+        ctx.use_stroke = false;
+        ctx.use_fill   = true;
         ctx.circle(
             p6::Center{_pos.x, _pos.y},
-            p6::Radius{_radius}
+            p6::Radius{_draw_radius}
         );
-        ctx.use_stroke = true;
-        ctx.use_fill   = false;
-        ctx.circle(
-            p6::Center{_pos.x, _pos.y},
-            p6::Radius{_cohesion_radius}
-        );
+        // avoid circle
+        if (show_avoid_circle)
+        {
+            ctx.stroke_weight = 0.002f;
+            ctx.use_stroke    = true;
+            ctx.use_fill      = false;
+            ctx.circle(
+                p6::Center{_pos.x, _pos.y},
+                p6::Radius{avoid_radius}
+            );
+        }
+        // cohesion circle
+        if (show_align_circle)
+        {
+            ctx.stroke_weight = 0.0025f;
+            ctx.use_stroke    = true;
+            ctx.use_fill      = false;
+            ctx.circle(
+                p6::Center{_pos.x, _pos.y},
+                p6::Radius{align_radius}
+            );
+        }
     }
-
-    //// collision
-    bool collides_with(const Boid& other) const
-    {
-        return glm::distance(_pos, other._pos) < (_radius + other._radius); // sqrt((_pos.x - other._pos.x)^2 + (_pos.y - other._pos.y)^2)
-    }
-
-    //// cohesion
-    // glm::vec2 cohesion(const std::vector<Boid>& boids, float cohesion_radius, float max_speed)
-    // {
-    //     glm::vec2 center_of_mass(0.0f, 0.0f);
-    //     int       count = 0;
-    //     for (const auto& other : boids)
-    //     {
-    //         if (&other != this && glm::distance(_pos, other._pos) < cohesion_radius)
-    //         {
-    //             center_of_mass += other._pos;
-    //             count++;
-    //         }
-    //     }
-    //     if (count > 0)
-    //     {
-    //         center_of_mass /= static_cast<float>(count);
-    //         auto desired_velocity = glm::normalize(center_of_mass - _pos) * max_speed;
-    //         return (desired_velocity - _speed);
-    //     }
-    //     return {0.0f, 0.0f};
-    // }
 
     //// update
-    void updatePosition(std::vector<Boid>& boids, float speed_multiplicator, float min_speed, float max_speed, float screen_margin, float aspect_ratio)
+    void updatePosition(float aspect_ratio, std::vector<Boid>& boids, float min_speed, float max_speed, float turn_factor, float avoid_radius, float avoid_factor, float align_radius, float align_factor, float speed_multiplier, float screen_margin)
     {
+        _turn_factor  = turn_factor;
+        _avoid_radius = avoid_radius;
+        _avoid_factor = avoid_factor;
+        _align_radius = align_radius;
+        _align_factor = align_factor;
         //////////////////////////////////////////////////
         // LIMITS OF THE SCREEN
         //////////////////////////////////////////////////
@@ -86,7 +82,9 @@ public:
         //     _pos.y = _pos.y * -1;
 
         // 2) Make them turn away !
-        float calculated_turn_factor = _turn_factor * speed_multiplicator;
+        float calculated_turn_factor  = _turn_factor * speed_multiplier;
+        float calculated_avoid_factor = _avoid_factor * speed_multiplier;
+        float calculated_align_factor = _align_factor * speed_multiplier;
         // Right
         if (_pos.x > aspect_ratio - screen_margin * aspect_ratio)
         {
@@ -109,6 +107,35 @@ public:
         }
 
         //////////////////////////////////////////////////
+        // AVOIDANCE & ALIGNMENT
+        //////////////////////////////////////////////////
+        glm::vec2 avoid_vec = {0, 0};
+        glm::vec2 align_vec = {0, 0};
+        int       friends   = 0;
+        for (auto& other : boids)
+        {
+            if (_pos != other._pos) // if not itself
+            {
+                float distance_to_other = glm::distance(_pos, other._pos);
+                if (distance_to_other < _avoid_radius) // if in avoid radius -> avoid
+                {
+                    avoid_vec += _pos - other._pos;
+                }
+                else if (distance_to_other < _align_radius) // else if in align radius
+                {
+                    align_vec += other._speed;
+                    friends += 1;
+                }
+            }
+        }
+        _speed += avoid_vec * calculated_avoid_factor;
+        if (friends > 0)
+        {
+            align_vec /= friends;
+            _speed += (align_vec - _speed) * calculated_align_factor;
+        }
+
+        //////////////////////////////////////////////////
         // MIN AND MAX SPEED
         //////////////////////////////////////////////////
         // updating the speed to have a min and max speed
@@ -128,26 +155,8 @@ public:
         // POSITION UPDATE
         //////////////////////////////////////////////////
         // last thing : updating the position from the speed
-        _pos.x += _speed.x * speed_multiplicator;
-        _pos.y += _speed.y * speed_multiplicator;
-
-        // Calculate cohesion steering force
-        // auto cohesion_force = cohesion(boids, _cohesion_radius, _max_speed);
-        // _speed += cohesion_force;
-        // if (glm::length(_speed) > 0.01)
-        // {                                                                              // calculate the norm of the vector speed
-        //     _dir = p6::Angle(static_cast<p6::Radians>(glm::atan(_speed.y, _speed.x))); // inverse the argument ???
-        // }
-
-        /*
-        // Check for collisions with other boids
-        for (auto& other : boids) {
-            if (&other != this && collides_with(other)) {
-                // Steer away from the other boid
-                auto diff = _pos - other._pos;
-                _dir += p6::Angle(static_cast<p6::Radians>(glm::atan(diff.y, diff.x)));
-            }
-        }*/
+        _pos.x += _speed.x * speed_multiplier;
+        _pos.y += _speed.y * speed_multiplier;
     }
 };
 
@@ -170,29 +179,62 @@ int main()
         Boid_array.push_back(boidTemp);
     }
 
-    float speed_multiplicator = 1;
-    float min_speed           = 30;
-    float max_speed           = 60;
-    float screen_margin       = .1;
+    // Boid parameters
+    float min_speed    = 20.; // to divide by 10000 /!\ not tunable in gui anymore
+    float max_speed    = 30.; // to divide by 10000 /!\ not tunable in gui anymore
+    float turn_factor  = 2.;  // to divide by 10000
+    float avoid_radius = 6.;  // to divide by 100
+    float avoid_factor = 2.;  // to divide by 1000
+    float align_radius = 20.; // to divide by 100
+    float align_factor = 6.;  // to divide by 1000
+    // Environment
+    float speed_multiplier = 1.; // to accelerate the simulation
+    float screen_margin    = .1; // to reduce the limits boids can't cross (default is full window)
+    // Helpers
+    bool show_avoid_circle = true;
+    bool show_align_circle = true;
 
     // Declare your infinite update loop.
     ctx.update = [&]() {
         ctx.background(p6::NamedColor::Blue);
 
         ///////RENDER
-        // Show a simple window
-        ImGui::Begin("Test");
-        ImGui::SliderFloat("Speed Multiplicator", &speed_multiplicator, 1, 10);
+        ImGui::Begin("Control Panel");
+
+        ImGui::Text("Boid parameters");
         // ImGui::SliderFloat("Min Speed", &min_speed, 0, 30);
         // ImGui::SliderFloat("Max Speed", &max_speed, 40, 70);
-        ImGui::SliderFloat("Screen Margin", &screen_margin, .1, 1);
+        ImGui::SliderFloat("Turn factor", &turn_factor, 1, 6);
+        ImGui::SliderFloat("Avoid radius", &avoid_radius, 0, 6);
+        ImGui::SliderFloat("Avoid factor", &avoid_factor, 0, 2);
+        ImGui::SliderFloat("Align radius", &align_radius, 0, 20);
+        ImGui::SliderFloat("Align factor", &align_factor, 0, 6);
+
+        ImGui::Text("Environment");
+        ImGui::SliderFloat("Speed multiplier", &speed_multiplier, 1, 10);
+        ImGui::SliderFloat("Screen margin", &screen_margin, .1, 1);
+
+        ImGui::Text("Helpers");
+        ImGui::Checkbox("Show avoid circles", &show_avoid_circle);
+        ImGui::Checkbox("Show align circles", &show_align_circle);
         ImGui::End();
 
         // Draw array
         for (auto& i : Boid_array)
         {
-            i.draw(ctx);
-            i.updatePosition(Boid_array, speed_multiplicator, min_speed / 10000, max_speed / 10000, screen_margin, ctx.aspect_ratio());
+            turn_factor  = turn_factor / 10000;
+            avoid_radius = avoid_radius / 100;
+            avoid_factor = avoid_factor / 1000;
+            align_radius = align_radius / 100;
+            align_factor = align_factor / 1000;
+            i.draw(ctx, show_avoid_circle, show_align_circle, avoid_radius, align_radius);
+            i.updatePosition(ctx.aspect_ratio(), Boid_array, min_speed, max_speed, turn_factor, avoid_radius, avoid_factor, align_radius, align_factor, speed_multiplier, screen_margin);
+            // il faut améliorer la fonction update position,
+            // il faudrait lui donner un objet "parameters" ça serait bcp + propre
+            // il faudrait SURTOUT faire une fonction qui update les boids avec les parametres
+            //      en gros séparer l'update des parametres des boids via la GUI
+            //      ET l'update de la position en fonction de ces paramètres
+            //      séparer en différente fonction tout ce qu'il se passe quoi
         }
     };
 
