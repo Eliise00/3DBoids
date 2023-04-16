af2050f4ea052251ad0ab456c537f6e1571da84b
Elise Massa <elise.massa.perso@gmail.com>
2023-04-16 19:28:34 +0200

    header Boid3D done

---
+++ Boid3D/Boid3D.h
@@ -0,0 +1,109 @@
+#include <cstdlib>
+#include <iostream>
+#include <random>
+#include "p6/p6.h"
+
+struct Boid_behavior_params {
+    float align_factor;
+    float align_radius;
+    float avoid_factor;
+    float avoid_radius;
+    float draw_radius;
+    float max_speed;
+    float min_speed;
+};
+
+struct Environment_params {
+    float speed_multiplier;
+    float aspect_ratio;
+    float screen_margin;
+    bool  show_align_circle;
+    bool  show_avoid_circle;
+    float z_limit;
+};
+
+class Boid3D {
+private:
+    glm::vec3 _pos;
+    glm::vec3 _speed;
+
+public:
+    explicit Boid3D(glm::vec3 initial_position)
+        : _pos(initial_position), //
+        _speed(glm::vec3(p6::random::number(-.001, .001),
+                         p6::random::number(-.001, .001),
+                         p6::random::number(-.001, .001)))
+    {
+    }
+
+    void adaptSpeedToBorders(Environment_params environment, Boid_behavior_params params)
+    {
+        float turn_factor = (params.avoid_factor * environment.speed_multiplier) / 10000;
+        if (_pos.x > environment.aspect_ratio - environment.screen_margin * environment.aspect_ratio) // Right
+            _speed.x -= turn_factor;
+
+        else if (_pos.x < -environment.aspect_ratio + environment.screen_margin * environment.aspect_ratio) // Left
+            _speed.x += turn_factor;
+
+        if (_pos.y > 1 - environment.screen_margin) // Top
+            _speed.y -= turn_factor;
+
+        else if (_pos.y < -1 + environment.screen_margin) // Bottom
+            _speed.y += turn_factor;
+
+        if (_pos.z > environment.z_limit) // Front
+            _speed.z -= turn_factor;
+
+        else if (_pos.z < -environment.z_limit) // Back
+            _speed.z += turn_factor;
+    }
+
+    void adaptSpeedToBoids(std::vector<Boid3D>& boids, Environment_params environment, Boid_behavior_params params)
+    {
+        glm::vec3 avoid_vec = glm::vec3(0, 0, 0);
+        glm::vec3 align_vec = glm::vec3(0, 0, 0);
+        int friends = 0;
+        for (auto& other : boids)
+        {
+            if (_pos != other._pos) // if not itself
+            {
+                float distance_to_other = glm::distance(_pos, other._pos);
+                if (distance_to_other < (params.avoid_radius / 100)) // if in avoid radius -> avoid
+                {
+                    avoid_vec += _pos - other._pos;
+                }
+                else if (distance_to_other < (params.align_radius / 100)) // else if in align radius
+                {
+                    align_vec += other._speed;
+                    friends += 1;
+                }
+            }
+        }
+        _speed += avoid_vec * ((params.avoid_factor * environment.speed_multiplier) / 1000);
+        if (friends > 0)
+        {
+            align_vec /= friends;
+            _speed += (align_vec - _speed) * ((params.align_factor * environment.speed_multiplier) / 1000);
+        }
+    }
+
+    void clampSpeed(Boid_behavior_params params)
+    {
+        float max_speed = params.max_speed / 10000;
+        float min_speed = params.min_speed / 10000;
+        float actual_speed = glm::length(_speed);
+        if (actual_speed > max_speed)
+        {
+            _speed = glm::normalize(_speed) * max_speed;
+        }
+        else if (actual_speed < min_speed)
+        {
+            _speed = glm::normalize(_speed) * min_speed;
+        }
+    }
+
+    void updatePosition(Environment_params environment)
+    {
+        _pos += _speed * environment.speed_multiplier;
+    }
+};
--- Boid3D/main.cpp
+++ Boid3D/main.cpp
@@ -6,7 +6,8 @@
 #include "glm/gtc/random.hpp"
 #include "glm/gtc/type_ptr.hpp"
 #include "img/src/Image.h"
-#include "p6/p6.h"
+
+#include "Boid3D.h"
 
 int const window_width  = 1920;
 int const window_height = 1080;
@@ -45,110 +46,7 @@ struct PenguinProgram {
     }
 };
 
-struct Boid_behavior_params {
-    float align_factor;
-    float align_radius;
-    float avoid_factor;
-    float avoid_radius;
-    float draw_radius;
-    float max_speed;
-    float min_speed;
-};
 
-struct Environment_params {
-    float speed_multiplier;
-    float aspect_ratio;
-    float screen_margin;
-    bool  show_align_circle;
-    bool  show_avoid_circle;
-    float z_limit;
-};
-
-class Boid3D {
-private:
-    glm::vec3 _pos;
-    glm::vec3 _speed;
-
-public:
-    explicit Boid3D(glm::vec3 initial_position)
-        : _pos(initial_position), //
-        _speed(glm::vec3(p6::random::number(-.001, .001),
-                         p6::random::number(-.001, .001),
-                         p6::random::number(-.001, .001)))
-    {
-    }
-
-    void adaptSpeedToBorders(Environment_params environment, Boid_behavior_params params)
-    {
-        float turn_factor = (params.avoid_factor * environment.speed_multiplier) / 10000;
-        if (_pos.x > environment.aspect_ratio - environment.screen_margin * environment.aspect_ratio) // Right
-            _speed.x -= turn_factor;
-
-        else if (_pos.x < -environment.aspect_ratio + environment.screen_margin * environment.aspect_ratio) // Left
-            _speed.x += turn_factor;
-
-        if (_pos.y > 1 - environment.screen_margin) // Top
-            _speed.y -= turn_factor;
-
-        else if (_pos.y < -1 + environment.screen_margin) // Bottom
-            _speed.y += turn_factor;
-
-        if (_pos.z > environment.z_limit) // Front
-            _speed.z -= turn_factor;
-
-        else if (_pos.z < -environment.z_limit) // Back
-            _speed.z += turn_factor;
-    }
-
-    void adaptSpeedToBoids(std::vector<Boid3D>& boids, Environment_params environment, Boid_behavior_params params)
-    {
-        glm::vec3 avoid_vec = glm::vec3(0, 0, 0);
-        glm::vec3 align_vec = glm::vec3(0, 0, 0);
-        int friends = 0;
-        for (auto& other : boids)
-        {
-            if (_pos != other._pos) // if not itself
-            {
-                float distance_to_other = glm::distance(_pos, other._pos);
-                if (distance_to_other < (params.avoid_radius / 100)) // if in avoid radius -> avoid
-                {
-                    avoid_vec += _pos - other._pos;
-                }
-                else if (distance_to_other < (params.align_radius / 100)) // else if in align radius
-                {
-                    align_vec += other._speed;
-                    friends += 1;
-                }
-            }
-        }
-        _speed += avoid_vec * ((params.avoid_factor * environment.speed_multiplier) / 1000);
-        if (friends > 0)
-        {
-            align_vec /= friends;
-            _speed += (align_vec - _speed) * ((params.align_factor * environment.speed_multiplier) / 1000);
-        }
-    }
-
-    void clampSpeed(Boid_behavior_params params)
-    {
-        float max_speed = params.max_speed / 10000;
-        float min_speed = params.min_speed / 10000;
-        float actual_speed = glm::length(_speed);
-        if (actual_speed > max_speed)
-        {
-            _speed = glm::normalize(_speed) * max_speed;
-        }
-        else if (actual_speed < min_speed)
-        {
-            _speed = glm::normalize(_speed) * min_speed;
-        }
-    }
-
-    void updatePosition(Environment_params environment)
-    {
-        _pos += _speed * environment.speed_multiplier;
-    }
-};
 
 
 void drawSphere(int i, const PenguinProgram& penguinProgram, const std::vector<glimac::ShapeVertex>& sphereVec,
@@ -183,6 +81,43 @@ int main()
 {
     auto ctx = p6::Context{{window_width, window_height, "TP8_DirectionalLight"}};
     ctx.maximize_window();
+
+    // BOID PARAMETERS //
+
+    Boid_behavior_params small_boid_params{
+        .align_factor = 6.,
+        .align_radius = 20.,
+        .avoid_factor = 2.,
+        .avoid_radius = 6.,
+        .draw_radius  = .01,
+        .max_speed    = 30,
+        .min_speed    = 20};
+
+    Boid_behavior_params big_boid_params{
+        .align_factor = 12.,
+        .align_radius = 40.,
+        .avoid_factor = 4.,
+        .avoid_radius = 12.,
+        .draw_radius  = .08,
+        .max_speed    = 8,
+        .min_speed    = 6};
+
+    Boid_behavior_params maxi_boid_params{
+        .align_factor = 12.,
+        .align_radius = 40.,
+        .avoid_factor = 4.,
+        .avoid_radius = 2.,
+        .draw_radius  = .2,
+        .max_speed    = 4,
+        .min_speed    = 3};
+
+    Environment_params environment_params{
+        .speed_multiplier  = 1.,
+        .aspect_ratio      = ctx.aspect_ratio(),
+        .screen_margin     = .1,
+        .show_align_circle = true,
+        .show_avoid_circle = true};
+
 
     // BEGINNING OF MY INIT CODE//
 
@@ -283,6 +218,17 @@ int main()
         glClearColor(0.2f, 0.2f, 0.2f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        // Initialize boids with desired parameters
+        std::vector<Boid3D> boids;
+        glm::vec3 initial_position(1.0f, 2.0f, 3.0f);
+
+        for (int i = 0; i < 32; ++i) {
+
+            Boid3D boid(initial_position);
+            boids.push_back(boid);
+        }
+
+
         // BEGIN OF MY DRAW CODE//
 
         glBindVertexArray(vao);
@@ -293,6 +239,7 @@ int main()
         for (int i = 0; i < 32; i++){
             drawSphere(i, penguin, sphereVec, ViewMatrix, ProjMatrix, MVMatrix_penguin, NormalMatrix_penguin, Ka, Kd, Ks, Shininess);
         }
+
 
         glBindVertexArray(0);
 
